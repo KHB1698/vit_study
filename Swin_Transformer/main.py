0a14e71dc63f03e8daaab40b4860b9398ae69d1a
KHB1698 <khb1698@qq.com>
2021-12-07 16:27:15 +0800

    master

--- Swin_Transformer/main.py
+++ Swin_Transformer/main.py
@@ -22,7 +22,7 @@ class PatchMerging(nn.Layer):
         self.resolution = input_resolution
         self.dim = dim
         self.reduction = nn.Linear(4*dim, 2*dim)
-        self.norm = nn.LayerNorm(4*dim)  # ! 感觉有问题
+        self.norm = nn.LayerNorm(4*dim)
 
     def forward(self, x):
         h, w = self.resolution
@@ -44,11 +44,11 @@ class PatchMerging(nn.Layer):
 class Mlp(nn.Layer):
     def __init__(self, dim, mlp_ratio=4.0, dropout=0.):
         super().__init__()
-        self.fc1 = nn.Linear(dim, int(dim* mlp_ratio))
-        self.fc2 = nn.Linear(int(dim* mlp_ratio), dim)
+        self.fc1 = nn.Linear(dim, int(dim * mlp_ratio))
+        self.fc2 = nn.Linear(int(dim * mlp_ratio), dim)
         self.act = nn.GELU()
         self.dropout = nn.Dropout(dropout)
-        
+
     def forward(self, x):
         x = self.fc1(x)
         x = self.act(x)
@@ -56,5 +56,23 @@ class Mlp(nn.Layer):
         x = self.fc2(x)
         x = self.dropout(x)
         return x
-        
-        
+
+
+# windows_partition不需要学习，所以定义一个方法就行
+def windows_partition(x, window_size):
+    B, H, W, C = x.shape
+    x = x.reshape([B, H//window_size, window_size, W//window_size, window_size, C])
+    x = x.transpose([0, 1, 3, 2, 4, 5])
+    # [B, H/window_size, W/window_size, window_size, window_size, C]
+    x = x.reshape([[-1, window_size, window_size, C]])  # ? 注意这里的reshape，两个中括号
+    # [B*H/window_size*W/window_size, window_size, window_size, C]
+    return x
+
+
+# windows_partition反过程，把窗口拼接起来
+def windows_reverse(windows, window_size, H, W):
+    B = int(windows.shape[0] // (H/window_size * W/window_size))
+    x = windows.reshape([B, H//window_size, W//window_size, window_size, window_size, -1])
+    x = x.transpose([0, 1, 3, 2, 4, 5])
+    x = x.reshape([B, H, W, -1])
+    return x
